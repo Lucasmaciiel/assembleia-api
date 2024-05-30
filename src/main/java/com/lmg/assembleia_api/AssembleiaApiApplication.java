1a901239c05608bca34412c1740a3c0adec5d498
Lucas Maciel <lucasmacielgois@gmail.com>
2024-05-30 16:56:51 -0300

    created project, java 17 and spring boot 3.3.0

---
+++ src/main/java/com/lmg/assembleia_api/AssembleiaApiApplication.java
@@ -0,0 +1,13 @@
+package com.lmg.assembleia_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssembleiaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssembleiaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lmg/assembleia_api/AssembleiaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmg.assembleia_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssembleiaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
