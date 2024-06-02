60922a304e7267ba3b32c455bd03dab0e97ddfff
Lucasmaciiel <lucasmacielgois@gmail.com>
2024-06-02 18:47:02 -0300

    Cpf validation

--- src/main/java/com/lmg/assembleia_api/domain/dto/request/VotoRequest.java
+++ src/main/java/com/lmg/assembleia_api/domain/dto/request/VotoRequest.java
@@ -2,6 +2,7 @@ package com.lmg.assembleia_api.domain.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -17,6 +18,7 @@ import lombok.Setter;
 public class VotoRequest {
 
     @NotBlank(message = "O CPF é obrigatório")
+    @Size(min = 11, max = 11, message = "CPF deve conter 11 caracteres")
     private String cpf;
 
     @NotNull(message = "Opção é obrigatório")
